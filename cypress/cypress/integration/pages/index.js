dfd78fc6301d2a59f53f1bcfcaf4ae19939f9e34
Felipe Leyva <d.leyvad@uniandes.edu.co>
2022-05-08 20:12:35 -0500

    Ghost pages test completed

--- cypress/cypress/integration/pages/index.js
+++ cypress/cypress/integration/pages/index.js
@@ -1,5 +1,6 @@
 import AdminPage from "./AdminPage";
 import LoginPage from "./LoginPage";
 import TagsPage from "./TagsPage";
+import PagesPage from "./PagesPage";
 
-export { AdminPage, LoginPage, TagsPage };
+export { AdminPage, LoginPage, TagsPage, PagesPage };
