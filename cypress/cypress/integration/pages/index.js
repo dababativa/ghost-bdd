361e117a5651c738b6dc4370500a618fa9071d54
MafeGarciaM <mf.garciam1@uniandes.edu.co>
2022-05-08 20:21:02 -0500

    Update index.js

--- cypress/cypress/integration/pages/index.js
+++ cypress/cypress/integration/pages/index.js
@@ -2,5 +2,6 @@ import AdminPage from "./AdminPage";
 import LoginPage from "./LoginPage";
 import TagsPage from "./TagsPage";
 import PagesPage from "./PagesPage";
+import PostPage from "./PostPage";
 
 export { AdminPage, LoginPage, TagsPage, PagesPage };
