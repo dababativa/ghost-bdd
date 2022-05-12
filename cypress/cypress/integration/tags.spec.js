6af0b02a7a0f6b72096d2e98e04050115d9fbe09
dababativa <da.babativa@uniandes.edu.co>
2022-05-12 17:45:44 -0500

    feat: cypress tags screenshots

--- cypress/cypress/integration/tags.spec.js
+++ cypress/cypress/integration/tags.spec.js
@@ -25,6 +25,7 @@ describe('Tags functionalities', () => {
         cy.wait(2000);
     })
     it('Test create tag', () => {
+        cy.screenshot('create-tag-initial-screen-4.42')
         self.tagsPage.clickCreateTagButton();
         cy.wait(1000);
         self.tagsPage.typeTagName('New tag name');
@@ -32,16 +33,20 @@ describe('Tags functionalities', () => {
         self.tagsPage.typeTagSlug('newTagSlug');
         cy.wait(1000);
         self.tagsPage.typeTagDescription('This is the description of a new tag created using cypress');
+        cy.screenshot('create-tag-inputs-screen-4.42')
         cy.wait(1000);
         self.tagsPage.clickSaveTagButton();
+        cy.screenshot('create-tag-saved-screen-4.42')
         cy.wait(1000);
         self.adminPage.clickTagsButton();
+        cy.screenshot('create-tag-results-screen-4.42')
         cy.wait(1000);
         self.tagsPage.assertTagWithTagNameExists('New tag name');
         cy.wait(5000);
     })
 
     it('Test create delete tag', () => {
+        cy.screenshot('delete-tag-initial-screen-4.42')
         self.tagsPage.clickCreateTagButton();
         cy.wait(1000);
         self.tagsPage.typeTagName('Deletable tag');
@@ -49,9 +54,11 @@ describe('Tags functionalities', () => {
         self.tagsPage.typeTagSlug('slug');
         cy.wait(1000);
         self.tagsPage.typeTagDescription('This is the description of a new tag created that will then be deleted using cypress');
+        cy.screenshot('delete-tag-inputs-screen-4.42')
         cy.wait(1000);
         self.tagsPage.clickSaveTagButton();
         cy.wait(1000);
+        cy.screenshot('delete-tag-tags-screen-4.42')
         self.adminPage.clickTagsButton();
         cy.wait(1000);
         self.tagsPage.openTagWithSlug('slug');
@@ -61,14 +68,17 @@ describe('Tags functionalities', () => {
         self.tagsPage.clickCancelDeleteButton();
         cy.wait(1000);
         self.tagsPage.clickDeleteTagButton();
+        cy.screenshot('delete-tag-delete-screen-4.42')
         cy.wait(1000);
         self.tagsPage.clickConfirmDeleteButton();
+        cy.screenshot('delete-tag-results-screen-4.42')
         cy.wait(1000)
         self.tagsPage.assertTagWithTagNameToNotExist('Deletable tag');
         cy.wait(3000);
     })
 
     it('Test create edit tag', () => {
+        cy.screenshot('edit-tag-initial-screen-4.42')
         self.tagsPage.clickCreateTagButton();
         cy.wait(1000);
         self.tagsPage.typeTagName('Editable tag');
@@ -77,9 +87,11 @@ describe('Tags functionalities', () => {
         cy.wait(1000);
         self.tagsPage.typeTagDescription('This is the description of a new tag created that will then be edited using cypress');
         cy.wait(1000);
+        cy.screenshot('edit-tag-inputs-screen-4.42')
         self.tagsPage.clickSaveTagButton();
         cy.wait(1000);
         self.adminPage.clickTagsButton();
+        cy.screenshot('edit-tag-tags-screen-4.42')
         cy.wait(1000);
         self.tagsPage.openTagWithSlug('slug');
         cy.wait(1000);
@@ -90,15 +102,18 @@ describe('Tags functionalities', () => {
         self.tagsPage.typeTagDescription('This is the description of a new tag created that has been edited using cypress');
         cy.wait(1000);
         self.tagsPage.clickSaveTagButton();
+        cy.screenshot('edit-tag-edit-screen-4.42')
         cy.wait(1000);
         self.adminPage.clickTagsButton();
         cy.wait(1000);
+        cy.screenshot('edit-tag-results-screen-4.42')
         self.tagsPage.assertTagWithTagNameToNotExist('Editable tag');
         self.tagsPage.assertTagWithTagNameExists('Edited tag');
         cy.wait(5000);
     })
 
     it('Test create internal tag', () => {
+        cy.screenshot('create-internal-tag-initial-screen-4.42')
         self.tagsPage.clickCreateTagButton();
         cy.wait(1000);
         self.tagsPage.typeTagName('#Internal tag');
@@ -107,11 +122,14 @@ describe('Tags functionalities', () => {
         cy.wait(1000);
         self.tagsPage.typeTagDescription('This is the description of a new internal tag created using cypress');
         cy.wait(1000);
+        cy.screenshot('create-internal-tag-inputs-screen-4.42')
         self.tagsPage.clickSaveTagButton();
+        cy.screenshot('create-internal-tag-saved-screen-4.42')
         cy.wait(1000);
         self.adminPage.clickTagsButton();
         cy.wait(1000);
         self.adminPage.navigateToInternalTags();
+        cy.screenshot('create-internal-tag-results-screen-4.42')
         self.tagsPage.assertTagWithTagNameExists('#Internal tag');
         cy.wait(5000);
     })
