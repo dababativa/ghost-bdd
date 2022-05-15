3f59a5e9bac5134635328b8b8f2a6e256c9499a8
MafeGarciaM <mf.garciam1@uniandes.edu.co>
2022-05-15 04:59:29 -0500

    Feat: Ajuste a capturas de pantalla para manejo de post (Cypress)

--- cypress/cypress/integration/post.spec.js
+++ cypress/cypress/integration/post.spec.js
@@ -28,6 +28,8 @@ describe('Testing posts Managements', () => {
     //Every it element is the 'then' of the Given-then-when process
 
     it('Test create post', () => {
+        cy.screenshot('create/1-post-list', {overwrite: false});
+        cy.wait(1000);
         self.postPage.clickCreatePostButton();
         cy.wait(1000);
         self.postPage.typePostTitle('Test 1 - Post');
@@ -36,28 +38,44 @@ describe('Testing posts Managements', () => {
         cy.wait(1000);
         self.postPage.clickPublishPostOptionButton();
         cy.wait(1000);
-        self.postPage.clickPublishPostButton();
+        cy.screenshot('create/2-post-to-create', {overwrite: false});
+        cy.wait(1000);        
+        self.postPage.clickPublishPostButton(); 
+        cy.wait(1000);       
+        cy.screenshot('create/3-post-confirm-creation', {overwrite: false});
         cy.wait(1000);
         self.postPage.clickConfirmPublishPostButton();
         cy.wait(1000);
-        self.postPage.clickBackToPostsButton();
+        self.postPage.clickBackToPostsButton();        
+        cy.wait(1000);
+        cy.screenshot('create/4-post-list-created', {overwrite: false});
         cy.wait(1000);
         self.postPage.assertPostExists('Test 1 - Post');
         cy.wait(5000);
     })
 
     it('Test delete post', () => {
+        cy.screenshot('delete/1-post-list', {overwrite: false});
+        cy.wait(1000); 
         self.postPage.clickPostTitleButton();
-        cy.wait(1000);
+        cy.wait(1000);        
+        cy.screenshot('delete/2-post-to-delete', {overwrite: false});
+        cy.wait(1000); 
         self.postPage.clickPostSettingsButton();
         cy.wait(1000);
         self.postPage.clickDeletePostButton();
+        cy.wait(1000); 
+        cy.screenshot('delete/3-post-delte', {overwrite: false});
         cy.wait(1000);
         self.postPage.clickConfirmDeletePostButton();
+        cy.wait(1000); 
+        cy.screenshot('delete/4-post-deleted', {overwrite: false});
         cy.wait(5000);
     })
 
     it('Test Edit post', () => {
+        cy.screenshot('edit/1-post-list', {overwrite: false});
+        cy.wait(1000);
         self.postPage.clickCreatePostButton();
         cy.wait(1000);
         self.postPage.typePostTitle('Test 2 - Post');
@@ -74,21 +92,31 @@ describe('Testing posts Managements', () => {
         cy.wait(1000);
         self.postPage.clickPostTitleButton();
         cy.wait(1000);
+        cy.screenshot('edit/2-post-non-edited', {overwrite: false});
+        cy.wait(1000);
         self.postPage.typePostTitle('Test 3 - Edites post');
         cy.wait(1000);
         self.postPage.typePostBody('body for post 3');
         cy.wait(1000);
         self.postPage.clickPublishPostOptionButton();
         cy.wait(1000);
+        cy.screenshot('edit/3-post-edited', {overwrite: false});
+        cy.wait(1000);
         self.postPage.clickPublishPostButton();
         cy.wait(1000);
+        cy.screenshot('edit/4-post-updated', {overwrite: false});
+        cy.wait(1000);
         self.postPage.clickBackToPostsButton();
+        cy.wait(1000);
+        cy.screenshot('edit/5-post-list', {overwrite: false});
         cy.wait(1000);
         self.postPage.assertPostExists('Test 3 - Edites post');
         cy.wait(5000);
     })
 
     it('Test Shedule post', () => {
+        cy.screenshot('schedule/1-post-list', {overwrite: false});
+        cy.wait(1000);
         self.postPage.clickCreatePostButton();
         cy.wait(1000);
         self.postPage.typePostTitle('Test 4 - Post');
@@ -97,17 +125,27 @@ describe('Testing posts Managements', () => {
         cy.wait(1000);
         self.postPage.clickPublishPostOptionButton();
         cy.wait(1000);
+        cy.screenshot('schedule/2-post-to-publish', {overwrite: false});
+        cy.wait(1000);
         self.postPage.clickCalendarButton();
         cy.wait(1000);
         self.postPage.clickDateCalendar();
+        cy.wait(1000);
+        cy.screenshot('schedule/3-post-date', {overwrite: false});
         cy.wait(1000);
         self.postPage.clickPublishPostButton();
         cy.wait(1000);
         self.postPage.clickConfirmPublishPostButton();
         cy.wait(1000);
+        cy.screenshot('schedule/4-post-schediled', {overwrite: false});
+        cy.wait(1000);
         self.postPage.clickBackToPostsButton();
         cy.wait(1000);
+        cy.screenshot('schedule/5-post-list', {overwrite: false});
+        cy.wait(1000);
         self.postPage.clickShedulePost();
+        cy.wait(1000);
+        cy.screenshot('schedule/6-post-sceduled-list', {overwrite: false});
         cy.wait(1000);
         self.postPage.assertPostExists('Test 4 - Post');
         cy.wait(5000);
