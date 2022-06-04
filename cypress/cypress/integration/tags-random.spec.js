0c46758d98b7a3f3595def141a14904a36565731
dababativa <da.babativa@uniandes.edu.co>
2022-06-04 18:34:30 -0500

    Feature: random

--- cypress/cypress/integration/tags-random.spec.js
+++ cypress/cypress/integration/tags-random.spec.js
@@ -1,8 +1,8 @@
 import { AdminPage, LoginPage, TagsPage } from "./pages";
 
 const faker = require("faker");
-faker.seed(2062022);
-describe("Tags functionalities pseudo", () => {
+// faker.seed(2062022)
+describe("Tags functionalities random", () => {
   before(() => {
     cy.visit(Cypress.env("login_url"));
     self.loginPage = new LoginPage();
@@ -39,34 +39,28 @@ describe("Tags functionalities pseudo", () => {
     self.adminPage.clickTagsButton();
     cy.wait(2000);
   });
-  it("Test create tag description over limit", () => {
-    const tag = self.data[0];
+
+  it("Test edit tag strange name", () => {
+    const tag = self.data[5];
     self.tagsPage.clickCreateTagButton();
     cy.wait(1000);
-    self.tagsPage.typeTagName(tag.name);
+    self.tagsPage.typeTagName("Editable tag");
     cy.wait(1000);
-    self.tagsPage.typeTagColor(tag.color.substring(1));
+    self.tagsPage.typeTagSlug("slug");
     cy.wait(1000);
-    self.tagsPage.typeTagSlug(tag.slug);
-    cy.wait(1000);
-    const descriptionLength = tag.description.length;
-    const descriptionModule = parseInt(500 / descriptionLength) + 1;
-    const description = tag.description.repeat(descriptionModule);
-    self.tagsPage.typeTagDescription(description);
+    self.tagsPage.typeTagDescription(
+      "This is the description of a new tag created that will then be edited using cypress"
+    );
     cy.wait(1000);
     self.tagsPage.clickSaveTagButton();
     cy.wait(1000);
-  });
-  it("Test create tag name on limit", () => {
-    const tag = self.data[1];
-    self.tagsPage.clickCreateTagButton();
+    self.adminPage.clickTagsButton();
     cy.wait(1000);
-    const nameLength = tag.name.length;
-    const nameModule = parseInt(191 / nameLength) + 1;
-    const name = tag.name.repeat(nameModule).substring(0, 192);
-    self.tagsPage.typeTagName(name);
+    self.tagsPage.openTagWithSlug("slug");
     cy.wait(1000);
-    self.tagsPage.typeTagColor(tag.color.substring(1));
+    self.tagsPage.typeTagName(
+      tag.name + "!@#$%^&*()-_=+{}[]|\\''\"\";:.>,</?`~áéíóú´"
+    );
     cy.wait(1000);
     self.tagsPage.typeTagSlug(tag.slug);
     cy.wait(1000);
@@ -74,56 +68,75 @@ describe("Tags functionalities pseudo", () => {
     cy.wait(1000);
     self.tagsPage.clickSaveTagButton();
     cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(5000);
   });
-  it("Test create tag slug on limit", () => {
-    const tag = self.data[2];
+
+  it("Test edit tag strange slug", () => {
+    const tag = self.data[5];
     self.tagsPage.clickCreateTagButton();
+    cy.wait(1000);
+    self.tagsPage.typeTagName("Editable tag");
+    cy.wait(1000);
+    self.tagsPage.typeTagSlug("slug");
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(
+      "This is the description of a new tag created that will then be edited using cypress"
+    );
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(1000);
+    self.tagsPage.openTagWithSlug("slug");
     cy.wait(1000);
     self.tagsPage.typeTagName(tag.name);
     cy.wait(1000);
-    self.tagsPage.typeTagColor(tag.color.substring(1));
+    self.tagsPage.typeTagSlug(
+      tag.slug + "!@#$%^&*()-_=+{}[]|\\''\"\";:.>,</?`~áéíóú´"
+    );
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(tag.description);
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(5000);
+  });
+
+  it("Test edit tag slug over limit", () => {
+    const tag = self.data[5];
+    self.tagsPage.clickCreateTagButton();
+    cy.wait(1000);
+    self.tagsPage.typeTagName("Editable tag");
+    cy.wait(1000);
+    self.tagsPage.typeTagSlug("slug");
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(
+      "This is the description of a new tag created that will then be edited using cypress"
+    );
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(1000);
+    self.tagsPage.openTagWithSlug("slug");
+    cy.wait(1000);
+    self.tagsPage.typeTagName(tag.name);
     cy.wait(1000);
     const slugLength = tag.slug.length;
     const slugModule = parseInt(191 / slugLength) + 1;
-    const slug = tag.slug.repeat(slugModule).substring(0, 192);
+    const slug = tag.slug.repeat(slugModule);
     self.tagsPage.typeTagSlug(slug);
     cy.wait(1000);
     self.tagsPage.typeTagDescription(tag.description);
     cy.wait(1000);
     self.tagsPage.clickSaveTagButton();
     cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(5000);
   });
-
-  it("Test create tag description on limit", () => {
-    const tag = self.data[3];
-    self.tagsPage.clickCreateTagButton();
-    cy.wait(1000);
-    self.tagsPage.typeTagName(tag.name);
-    cy.wait(1000);
-    self.tagsPage.typeTagColor(tag.color.substring(1));
-    cy.wait(1000);
-    self.tagsPage.typeTagSlug(tag.slug);
-    cy.wait(1000);
-    const descriptionLength = tag.description.length;
-    const descriptionModule = parseInt(500 / descriptionLength) + 1;
-    const description = tag.description
-      .repeat(descriptionModule)
-      .substring(0, 501);
-    self.tagsPage.typeTagDescription(description);
-    cy.wait(1000);
-    self.tagsPage.clickSaveTagButton();
-    cy.wait(1000);
-  });
-
-  it("Test create tag no data", () => {
-    const tag = self.data[4];
-    self.tagsPage.clickCreateTagButton();
-    cy.wait(1000);
-    self.tagsPage.clickSaveTagButton();
-    cy.wait(1000);
-  });
-
-  it("Test edit tag correctly", () => {
+  it("Test edit tag description over limit", () => {
     const tag = self.data[5];
     self.tagsPage.clickCreateTagButton();
     cy.wait(1000);
@@ -145,15 +158,18 @@ describe("Tags functionalities pseudo", () => {
     cy.wait(1000);
     self.tagsPage.typeTagSlug(tag.slug);
     cy.wait(1000);
-    self.tagsPage.typeTagDescription(tag.description);
+    const descriptionLength = tag.description.length;
+    const descriptionModule = parseInt(501 / descriptionLength) + 1;
+    const description = tag.description.repeat(descriptionModule);
+    self.tagsPage.typeTagDescription(description);
     cy.wait(1000);
     self.tagsPage.clickSaveTagButton();
     cy.wait(1000);
     self.adminPage.clickTagsButton();
     cy.wait(5000);
   });
-  it("Test edit tag no name", () => {
-    const tag = self.data[6];
+  it("Test edit tag name over limit", () => {
+    const tag = self.data[5];
     self.tagsPage.clickCreateTagButton();
     cy.wait(1000);
     self.tagsPage.typeTagName("Editable tag");
@@ -169,88 +185,166 @@ describe("Tags functionalities pseudo", () => {
     self.adminPage.clickTagsButton();
     cy.wait(1000);
     self.tagsPage.openTagWithSlug("slug");
-    cy.wait(1000);
-    self.tagsPage.typeTagName("");
-    cy.wait(1000);
-    self.tagsPage.typeTagSlug(tag.slug);
-    cy.wait(1000);
-    self.tagsPage.typeTagDescription(tag.description);
-    cy.wait(1000);
-    self.tagsPage.clickSaveTagButton();
-    cy.wait(5000);
-  });
-  it("Test edit tag no slug", () => {
-    const tag = self.data[7];
-    self.tagsPage.clickCreateTagButton();
-    cy.wait(1000);
-    self.tagsPage.typeTagName("Editable tag");
-    cy.wait(1000);
-    self.tagsPage.typeTagSlug("slug");
-    cy.wait(1000);
-    self.tagsPage.typeTagDescription(
-      "This is the description of a new tag created that will then be edited using cypress"
-    );
-    cy.wait(1000);
-    self.tagsPage.clickSaveTagButton();
-    cy.wait(1000);
-    self.adminPage.clickTagsButton();
-    cy.wait(1000);
-    self.tagsPage.openTagWithSlug("slug");
-    cy.wait(1000);
-    self.tagsPage.typeTagName(tag.name);
-    cy.wait(1000);
-    self.tagsPage.typeTagSlug("");
-    cy.wait(1000);
-    self.tagsPage.typeTagDescription(tag.description);
-    cy.wait(1000);
-    self.tagsPage.clickSaveTagButton();
-    cy.wait(5000);
-  });
-  it("Test edit tag no description", () => {
-    const tag = self.data[8];
-    self.tagsPage.clickCreateTagButton();
-    cy.wait(1000);
-    self.tagsPage.typeTagName("Editable tag");
-    cy.wait(1000);
-    self.tagsPage.typeTagSlug("slug");
-    cy.wait(1000);
-    self.tagsPage.typeTagDescription(
-      "This is the description of a new tag created that will then be edited using cypress"
-    );
-    cy.wait(1000);
-    self.tagsPage.clickSaveTagButton();
-    cy.wait(1000);
-    self.adminPage.clickTagsButton();
-    cy.wait(1000);
-    self.tagsPage.openTagWithSlug("slug");
-    cy.wait(1000);
-    self.tagsPage.typeTagName(tag.name);
-    cy.wait(1000);
-    self.tagsPage.typeTagSlug(tag.slug);
-    cy.wait(1000);
-    self.tagsPage.typeTagDescription("");
-    cy.wait(1000);
-    self.tagsPage.clickSaveTagButton();
-    cy.wait(5000);
-  });
-
-  it("Test create tag name over limit", () => {
-    const tag = self.data[9];
-    self.tagsPage.clickCreateTagButton();
     cy.wait(1000);
     const nameLength = tag.name.length;
     const nameModule = parseInt(191 / nameLength) + 1;
     const name = tag.name.repeat(nameModule);
-    cy.log(name);
     self.tagsPage.typeTagName(name);
-    cy.wait(1000);
-    self.tagsPage.typeTagColor(tag.color.substring(1));
     cy.wait(1000);
     self.tagsPage.typeTagSlug(tag.slug);
     cy.wait(1000);
     self.tagsPage.typeTagDescription(tag.description);
     cy.wait(1000);
     self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
     cy.wait(5000);
+  });
+
+  it("Test edit tag slug on limit", () => {
+    const tag = self.data[5];
+    self.tagsPage.clickCreateTagButton();
+    cy.wait(1000);
+    self.tagsPage.typeTagName("Editable tag");
+    cy.wait(1000);
+    self.tagsPage.typeTagSlug("slug");
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(
+      "This is the description of a new tag created that will then be edited using cypress"
+    );
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(1000);
+    self.tagsPage.openTagWithSlug("slug");
+    cy.wait(1000);
+    self.tagsPage.typeTagName(tag.name);
+    cy.wait(1000);
+    const slugLength = tag.slug.length;
+    const slugModule = parseInt(191 / slugLength) + 1;
+    const slug = tag.slug.repeat(slugModule).substring(0, 191);
+    self.tagsPage.typeTagSlug(slug);
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(tag.description);
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(5000);
+  });
+  it("Test edit tag description on limit", () => {
+    const tag = self.data[5];
+    self.tagsPage.clickCreateTagButton();
+    cy.wait(1000);
+    self.tagsPage.typeTagName("Editable tag");
+    cy.wait(1000);
+    self.tagsPage.typeTagSlug("slug");
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(
+      "This is the description of a new tag created that will then be edited using cypress"
+    );
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(1000);
+    self.tagsPage.openTagWithSlug("slug");
+    cy.wait(1000);
+    self.tagsPage.typeTagName(tag.name);
+    cy.wait(1000);
+    self.tagsPage.typeTagSlug(tag.slug);
+    cy.wait(1000);
+    const descriptionLength = tag.description.length;
+    const descriptionModule = parseInt(501 / descriptionLength) + 1;
+    const description = tag.description
+      .repeat(descriptionModule)
+      .substring(0, 500);
+    self.tagsPage.typeTagDescription(description);
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(5000);
+  });
+  it("Test edit tag name on limit", () => {
+    const tag = self.data[5];
+    self.tagsPage.clickCreateTagButton();
+    cy.wait(1000);
+    self.tagsPage.typeTagName("Editable tag");
+    cy.wait(1000);
+    self.tagsPage.typeTagSlug("slug");
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(
+      "This is the description of a new tag created that will then be edited using cypress"
+    );
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(1000);
+    self.tagsPage.openTagWithSlug("slug");
+    cy.wait(1000);
+    const nameLength = tag.name.length;
+    const nameModule = parseInt(191 / nameLength) + 1;
+    const name = tag.name.repeat(nameModule).substring(0, 191);
+    self.tagsPage.typeTagName(name);
+    cy.wait(1000);
+    self.tagsPage.typeTagSlug(tag.slug);
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(tag.description);
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(5000);
+  });
+  it("Test edit tag blank", () => {
+    const tag = self.data[5];
+    self.tagsPage.clickCreateTagButton();
+    cy.wait(1000);
+    self.tagsPage.typeTagName("Editable tag");
+    cy.wait(1000);
+    self.tagsPage.typeTagSlug("slug");
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(
+      "This is the description of a new tag created that will then be edited using cypress"
+    );
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(1000);
+    self.tagsPage.openTagWithSlug("slug");
+    cy.wait(1000);
+    self.tagsPage.typeTagName(" ");
+    cy.wait(1000);
+    self.tagsPage.typeTagSlug(" ");
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(" ");
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
+    self.adminPage.clickTagsButton();
+    cy.wait(5000);
+  });
+
+  it("Test create tag slug over limit", () => {
+    const tag = self.data[0];
+    self.tagsPage.clickCreateTagButton();
+    cy.wait(1000);
+    self.tagsPage.typeTagName(tag.name);
+    cy.wait(1000);
+    self.tagsPage.typeTagColor(tag.color.substring(1));
+    cy.wait(1000);
+    const slugLength = tag.slug.length;
+    const slugModule = parseInt(192 / slugLength) + 1;
+    const slug = tag.slug.repeat(slugModule);
+    self.tagsPage.typeTagSlug(slug);
+    cy.wait(1000);
+    self.tagsPage.typeTagDescription(tag.description);
+    cy.wait(1000);
+    self.tagsPage.clickSaveTagButton();
+    cy.wait(1000);
   });
 });
